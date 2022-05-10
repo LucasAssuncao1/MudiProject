a0e27180626b8aee95ec7ee929ddc85797c29a6a
Lucas Araujo De Assunção <lucas.assuncao@marinha.mil.br>
2022-05-10 14:10:12 -0300

    Trabalhando com Bean validation/exibindo mensagens de erro no formulário usando thymeleaf

--- mudi/src/main/java/br/com/alura/mvc/mudi/controller/PedidoController.java
+++ mudi/src/main/java/br/com/alura/mvc/mudi/controller/PedidoController.java
@@ -1,17 +1,43 @@
 package br.com.alura.mvc.mudi.controller;
 
+import javax.validation.Valid;
+
+import org.hibernate.annotations.common.util.StringHelper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import br.com.alura.mvc.mudi.dto.NovoPedidoRequest;
+import br.com.alura.mvc.mudi.model.Pedido;
+import br.com.alura.mvc.mudi.repository.PedidoRepository;
 
 @Controller
 @RequestMapping("/pedido")
 public class PedidoController {
 	
+	@Autowired
+	private PedidoRepository pedidoRepository;
+	
 	@GetMapping("/formulario")
-	public String formulario() {
+	public String formulario(NovoPedidoRequest novoPedidoRequest) {
 		return "pedido/formulario";
 	}
+	
+	@PostMapping("/novo")
+	public String novo(@Valid NovoPedidoRequest novoPedidoRequest, BindingResult result) {
+		if(result.hasErrors()) {
+			return "pedido/formulario";
+		}
+		
+		Pedido pedido = novoPedidoRequest.toPedido();
+		pedidoRepository.save(pedido);
+		
+		return "pedido/formulario";  
+	}
+	
 	
 
 }
