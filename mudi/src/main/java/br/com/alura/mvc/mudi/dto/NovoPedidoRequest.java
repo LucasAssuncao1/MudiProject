87c517d21d2aec8689f7df1e35984a01b49a98a7
Lucas Araujo De Assunção <lucas.assuncao@marinha.mil.br>
2022-05-20 15:24:08 -0300

    modelando status / rotas dos status

--- mudi/src/main/java/br/com/alura/mvc/mudi/dto/NovoPedidoRequest.java
+++ mudi/src/main/java/br/com/alura/mvc/mudi/dto/NovoPedidoRequest.java
@@ -3,6 +3,7 @@ package br.com.alura.mvc.mudi.dto;
 import javax.validation.constraints.NotBlank;
 
 import br.com.alura.mvc.mudi.model.Pedido;
+import br.com.alura.mvc.mudi.model.StatusPedido;
 
 public class NovoPedidoRequest {
 	
@@ -15,6 +16,8 @@ public class NovoPedidoRequest {
 	@NotBlank
 	private String urlImagem;
 	private String descricao;
+	
+	
 	
 	
 	public String getNomeProduto() {
@@ -48,10 +51,10 @@ public class NovoPedidoRequest {
 		pedido.setUrlProduto(urlProduto);
 		pedido.setUrlImagem(urlImagem);
 		pedido.setDescricao(descricao);
+		pedido.setStatus(StatusPedido.AGUARDANDO);
 		
 		return pedido;
 	}
-	
 	
 	
 
