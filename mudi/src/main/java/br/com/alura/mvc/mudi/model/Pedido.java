9395a4707c1361f950fb0bcb79b693a2f1cfed85
lucas <lucas@192.168.1.106>
2022-06-29 15:46:47 -0300

    trabalhando com vueJs

--- mudi/src/main/java/br/com/alura/mvc/mudi/model/Pedido.java
+++ mudi/src/main/java/br/com/alura/mvc/mudi/model/Pedido.java
@@ -2,7 +2,9 @@ package br.com.alura.mvc.mudi.model;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.util.List;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
@@ -11,6 +13,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 
 @Entity
 public class Pedido {
@@ -31,6 +34,9 @@ public class Pedido {
 	
 	@ManyToOne()
 	private User user;
+	
+	@OneToMany( cascade = CascadeType.ALL, mappedBy = "pedido", fetch = FetchType.LAZY)
+	private List<Oferta> ofertas;
 	
 
 	public String getNome() {
@@ -96,6 +102,14 @@ public class Pedido {
 
 	public void setUser(User user) {
 		this.user = user;
+	}
+	
+	public List<Oferta> getOfertas() {
+		return ofertas;
+	}
+
+	public void setOfertas(List<Oferta> ofertas) {
+		this.ofertas = ofertas;
 	}
 
 }
